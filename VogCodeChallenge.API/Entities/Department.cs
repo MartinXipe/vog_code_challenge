99ee7588a16e2b9c140cf004b8b99dfd41fac392
unknown <martin@xipetechnology.com>
2021-12-15 21:52:07 -0700

    entities were created

---
+++ VogCodeChallenge.API/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Department:Entity
+    {
+        public string Name { get; set; }
+        //[Index(IsUnique = true)]
+        public string Address { get; set; }
+    }
+}
---
+++ VogCodeChallenge.API/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Entity
+    {
+        public int Id { get;  private set; }
+        public DateTime CreationDate { get; private set; }
+    }
+}
